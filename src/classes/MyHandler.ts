322a83f240af94d648314814b3c29037b596fee9
idinium96 <idinium96@users.noreply.github.com>
2020-04-06 01:38:14 +0800

    fix some ugly looking offer summary

--- src/classes/MyHandler.ts
+++ src/classes/MyHandler.ts
@@ -664,7 +664,7 @@ export = class MyHandler extends Handler {
                     offer.id +
                     ' is waiting for review, reason: ' +
                     reason +
-                    '\nSummary:' +
+                    '\nOffer Summary:\n' +
                     offer.summarize(this.bot.schema)
             );
         }
