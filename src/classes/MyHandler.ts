8206f31eae39e42ba0c6dde69a544710a10859fa
idinium96 <idinium96@users.noreply.github.com>
2020-04-07 05:36:26 +0800

    little change

--- src/classes/MyHandler.ts
+++ src/classes/MyHandler.ts
@@ -629,7 +629,7 @@ export = class MyHandler extends Handler {
                     '\n\nNote:\n❌INVALID_VALUE - Ignored/Declined. You might choose the wrong item (Ex: Non-Craftable item has different value - https://prntscr.com/rpc3gn)' +
                     '\n⭕INVALID_ITEMS - Some item(s) you offered might not in my pricelist. Please wait for my boss to verify it.' +
                     "\n⭕OVERSTOCKED - Some item(s) you offered might already reached max amount I can have OR it's a common bug on me. Please wait until my boss verify it." +
-                    "\n⭕ESCROW - You're currently has a trade restriction. My boss will accept it if you offer the correct value. Please wait. In the future, please use Steam Guard Mobile Authenticator." +
+                    "\n⭕ESCROW - You're currently has a trade restriction. My boss will accept it if you offer the correct or overpay value. Please wait. In the future, please use Steam Guard Mobile Authenticator." +
                     '\n\n⚡Usually takes about 1-2 minutes or more or less for my boss to review it.' +
                     '\n\nMy boss active hours: 🕖0700H - 🕐0100H Malaysia Time (GMT +8) - https://www.timeanddate.com/worldclock/malaysia/kuala-lumpur' +
                     '\n\nIf you need any help, please contact my boss via Discord Server: https://discord.gg/AXTGF4g' +
