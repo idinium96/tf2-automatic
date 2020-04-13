8adb36684ca9b1379873906e77659b604c268e0f
idinium96 <idinium96@users.noreply.github.com>
2020-04-14 01:17:09 +0800

    update version

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // TODO: Update version for each release
-process.env.BOT_VERSION = '3.3.12-rm.1.1.0';
+process.env.BOT_VERSION = '3.3.12-rm.1.2.0';
 
 import fs from 'fs';
 import path from 'path';
