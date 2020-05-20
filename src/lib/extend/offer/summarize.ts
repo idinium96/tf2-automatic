4b42ac498c4b66014193abc979923063a47ca3aa
idinium96 <idinium96@users.noreply.github.com>
2020-05-21 06:56:53 +0800

    fix bot crashes when an admin sent an offer

--- src/lib/extend/offer/summarize.ts
+++ src/lib/extend/offer/summarize.ts
@@ -17,12 +17,11 @@ export = function(schema: SchemaManager.Schema): string {
         their: UnknownDictionary<number>;
     } = self.data('dict') || { our: null, their: null };
 
-    const valueDiff = new Currencies(value.their).toValue() - new Currencies(value.our).toValue();
-    const valueDiffRef = Currencies.toRefined(Currencies.toScrap(Math.abs(valueDiff * (1 / 9)))).toString();
-
     if (!value) {
         return 'Asked: ' + summarizeItems(items.our, schema) + 'Offered: ' + summarizeItems(items.their, schema);
     } else {
+        const valueDiff = new Currencies(value.their).toValue() - new Currencies(value.our).toValue();
+        const valueDiffRef = Currencies.toRefined(Currencies.toScrap(Math.abs(valueDiff * (1 / 9)))).toString();
         return (
             'Asked: ' +
             new Currencies(value.our).toString() +
