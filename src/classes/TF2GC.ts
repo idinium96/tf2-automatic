019179e1a1f69c0e4b43225319c6f1b504028267
idinium96 <idinium96@users.noreply.github.com>
2020-05-25 05:03:39 +0800

    nothing changed.. will always show running tf2.

--- src/classes/TF2GC.ts
+++ src/classes/TF2GC.ts
@@ -173,7 +173,7 @@ export = class TF2GC {
             () => {
                 this.bot.inventoryManager.getInventory().removeItem(job.assetid);
 
-                this.bot.client.gamesPlayed('tf2-automatic-using-item');
+                this.bot.client.gamesPlayed('tf2-automatic');
                 this.finishedProcessingJob();
             },
             err => {
