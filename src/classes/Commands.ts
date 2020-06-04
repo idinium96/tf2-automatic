5cba83423ceef695f8f7385707c0933ba00f9e6d
idinium96 <idin100msc@gmail.com>
2020-06-04 10:07:57 +0800

    add pure command

--- src/classes/Commands.ts
+++ src/classes/Commands.ts
@@ -29,6 +29,7 @@ const COMMANDS: string[] = [
     '!help - Get list of commands 📜',
     '!how2trade - Guide on how to use and trade with the bot 📋',
     '!stock - Get a list of items that the bot has 📊',
+    '!pure - Get current pure stock 💰',
     '!rate - Get current key prices 🔑',
     '!price [amount] <name> - Get the price and stock of an item 💱',
     '!message <Your Messages> - Send a message to the owner of the bot 💬',
@@ -90,6 +91,8 @@ export = class Commands {
             this.priceCommand(steamID, message);
         } else if (command === 'stock') {
             this.stockCommand(steamID);
+        } else if (command === 'pure') {
+            this.pureCommand(steamID);
         } else if (command === 'craftweapon' && isAdmin) {
             this.craftweaponCommand(steamID);
         } else if (command === 'message') {
@@ -162,6 +165,7 @@ export = class Commands {
             message.startsWith('💲') ||
             message.startsWith('📜') ||
             message.startsWith('🛒') ||
+            message.startsWith('💰') ||
             message.startsWith('Here') ||
             message.startsWith('The') || // or 'There'
             message.startsWith('Please') ||
@@ -377,6 +381,34 @@ export = class Commands {
         const reply = "📃 Here's a list of all craft weapons stock in my inventory:\n\n" + crafWeaponStock.join(', \n');
 
         this.bot.sendMessage(steamID, reply);
+    }
+
+    private pureCommand(steamID: SteamID): void {
+        const pureStock = this.pureStock();
+
+        this.bot.sendMessage(steamID, `💰 I have currently ${pureStock.join(' and ')} in my inventory.`);
+    }
+
+    private pureStock(): string[] {
+        const pureStock: string[] = [];
+        const pureScrap = this.bot.inventoryManager.getInventory().getAmount('5000;6') * (1 / 9);
+        const pureRec = this.bot.inventoryManager.getInventory().getAmount('5001;6') * (1 / 3);
+        const pureRef = this.bot.inventoryManager.getInventory().getAmount('5002;6');
+        const pureScrapTotal = Currencies.toScrap(pureRef + pureRec + pureScrap);
+        const pure = [
+            {
+                name: 'keys',
+                amount: this.bot.inventoryManager.getInventory().getAmount('5021;6')
+            },
+            {
+                name: 'ref',
+                amount: Currencies.toRefined(pureScrapTotal)
+            }
+        ];
+        for (let i = 0; i < pure.length; i++) {
+            pureStock.push(`${pure[i].amount} ${pure[i].name}`);
+        }
+        return pureStock;
     }
 
     private rateCommand(steamID: SteamID): void {
